1786298cf0595e05731b293060f78488ca062990
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-12-04 13:20:02 +1300

    Update index.js

--- index.js
+++ index.js
@@ -17,18 +17,72 @@ function TemperatureFileAccessory(log, config) {
 
   this.service
     .getCharacteristic(Characteristic.CurrentTemperature)
-    .on('get', this.getState.bind(this));
+    .on('get', this.getTemperature.bind(this));
+
+  this.state = readDataFile (this.filePath,log);
+
+  if(this.state.humidity){
+    this.service
+      .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+      .on('get', this.getHumidity.bind(this));
+  }
+
+  if(this.state.battery){
+    this.service
+      .getCharacteristic(Characteristic.StateLowBattery)
+      .on('get', this.getLowBattery.bind(this));
+  }
 }
 
-TemperatureFileAccessory.prototype.getState = function(callback) {
-  fs.readFile(this.filePath, 'utf8', function(err, data) {
-    if (err) {
-      callback(err);
-      return
-    }
+TemperatureFileAccessory.prototype.getTemperature = function(callback) {
+    const {log} = this;
+    this.state = readDataFile (this.filePath,log);
 
-    callback(null, parseFloat(data))
-  })
+    callback(null, parseFloat(this.state.temperature))
+}
+
+TemperatureFileAccessory.prototype.getHumidity = function(callback) {
+    const {log} = this;
+
+    callback(null, parseFloat(this.state.humidity))
+}
+
+TemperatureFileAccessory.prototype.getLowBattery = function(callback) {
+    const {log} = this;
+
+    let alert = (this.state.battery < 20) ? 1 : 0;
+    callback(null, alert)
+}
+
+function readDataFile (filePath,log) {
+    const state= {};
+
+    fs.readFile(filePath, 'utf8', function(err, data) {
+          if (err) {
+         log(`\x1b[31m[ERROR] \x1b[0m${name} updateTemperatureFromFile\n\n${err.message}`);
+      }
+
+      if (data === undefined || data.trim().length === 0) {
+        log(`\x1b[33m[WARNING]\x1b[0m ${name} Error reading file: ${this.filePath}, using previous Temperature`);
+        return;
+      }
+
+      const lines = data.split(/\r?\n/);
+      if (/^[0-9]+\.*[0-9]*$/.test(lines[0])){
+        state.temperature = parseFloat(data);
+      } else {
+        lines.forEach((line) => {
+          if(-1 < line.indexOf(':')){
+            let value = line.split(':');
+            if(value[0] == 'temperature') state.temperature = parseFloat(value[1]);
+            if(value[0] == 'humidity') state.humidity = parseFloat(value[1]);
+            if(value[0] == 'battery') state.battery = parseFloat(value[1]);
+          }
+        });
+      }
+    })
+
+  return state;
 }
 
 TemperatureFileAccessory.prototype.getServices = function() {
